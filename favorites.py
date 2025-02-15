a5c84473b224b869e9fa59efff4e2f923f13bff9
thebooleanguy <tharindus0601@gmail.com>
2025-02-15 16:07:19 +0530

    Update favorites.py

--- favorites.py
+++ favorites.py
@@ -1,1 +1,1 @@
-exceptions = ['islandposting', 'coverartmatters', 'pigsandplans', 'savingconnie', 'hoodfinch', 'kendricklamar', 'feliciathegoat', 'kidcudi', 'asaprocky', 'deedat.clothing', 'decant_services', 'tharuu__.shi', 'timdessaint', 'miziziziz', 'bigmoistcr1tikal']
+exceptions = ['islandposting', 'coverartmatters', 'pigsandplans', 'savingconnie', 'hoodfinch', 'kendricklamar', 'feliciathegoat', 'kidcudi', 'asaprocky', 'deedat.clothing', 'decant_services', 'timdessaint', 'miziziziz', 'bigmoistcr1tikal']
